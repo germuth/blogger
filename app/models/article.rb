00bb4ea095d79b6cc158a8f0dabc77c7720d4198
Aaron Germuth <germuth@unbc.ca>
2015-03-10 21:13:22 -0700

    Tagging of articles complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article < ActiveRecord::Base
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+		tags.join(", ")
+	end
+	
+	def tag_list= tags_string
+		tag_names = tags_string.split(",").collect do |s|
+			s.string.downcase
+		end.uniq
+
+		self.tags = tag_names.collect do |tag_name|
+			Tag.find_or_create_by name: tag_name
+		end
+	end
 end

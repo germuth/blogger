840abc20cd944a29e8d7763b10d19d27fb88fe3f
Aaron Germuth <germuth@unbc.ca>
2015-03-10 22:03:01 -0700

    added paperclip gem for attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type =>
+		["image/jpg", "image/jpeg", "image/png"]
 
 	def tag_list
 		tags.join(", ")
@@ -9,7 +12,7 @@ class Article < ActiveRecord::Base
 	
 	def tag_list= tags_string
 		tag_names = tags_string.split(",").collect do |s|
-			s.string.downcase
+			s.strip.downcase
 		end.uniq
 
 		self.tags = tag_names.collect do |tag_name|
